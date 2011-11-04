b6e1dc8722d0268ac30c13103fb87517975aa799
Frank Hebbert <frank@hebbert.com>
2011-11-04 15:23:49 -0400

    Reworked the cbu scraper to add some jitter to points.

--- scraperwiki.py
+++ scraperwiki.py
@@ -3,6 +3,7 @@ import lxml.html
 import json
 
 import sys
+import random
 
 def is_number(s):
     try:
@@ -13,7 +14,8 @@ def is_number(s):
 
 # first we get everything from ioby
 def ioby():
-    print "starting on ioby"
+
+    print "Getting ioby projects..."
 
     # starting links
     html = scraperwiki.scrape("http://ioby.org/projects?phrase=&status=All&vols=All&sort_by=title&sort_order=ASC&items_per_page=All")
@@ -62,18 +64,22 @@ def ioby():
 # scrape changebyus
 def changebyus():
 
+    print "Getting Change By Us projects..."
+
     offset = 50 # how big a chunk each time?
     start_items = 0
     max_items = 350 # how many to fetch in total? 
 
     # get each chunk of json
     for index in range(start_items, max_items, offset):
+        print "Fetching " + str(index)
         cbu_json_url = "http://nyc.changeby.us/search/projects?terms=&n=" + str(offset) + "&offset=" + str(index) #confusing use of offset/index, they are reversed
 
         cbu_json = scraperwiki.scrape(cbu_json_url)
         project_string = json.loads(cbu_json)
 
         for project in range(0,offset):
+            store_it = 1
 
             # deal with variable lengths in the returned json here - if it's the final tranche there may not be {offset} items
             try:
@@ -82,44 +88,50 @@ def changebyus():
                 break
 
             url = project_string["results"][project]["project_id"]
-
             project_url = "http://nyc.changeby.us/project/" + str(url)
-                
-            if url == "/project/":
-                break
+
+            if url != "/project/":
+
+                # go to subpage for the location details
+                subpage = scraperwiki.scrape(project_url) # this is a long page, we're looking for the map config info in a line containing app_page.data.project
     
-            # go to subpage for the location details
-            subpage = scraperwiki.scrape(project_url) # this is a long page, we're looking for the map config info in a line containing app_page.data.project
-    
-            start = subpage.find("app_page.data.project")
-            end = subpage.find("\n", start)
-            line = subpage[start:end]
+                start = subpage.find("app_page.data.project")
+                end = subpage.find("\n", start)
+                line = subpage[start:end]
 
-            # clean it up
-            projectjson = line.replace('app_page.data.project = ', '')[:-1]
+                # clean it up
+                projectjson = line.replace('app_page.data.project = ', '')[:-1]
 
-            bigjson = json.loads(projectjson)
+                bigjson = json.loads(projectjson)
 
-            # this is not very elegant, but I'm confused about dealing with this json-like string
-            project_lat = bigjson["info"]["location"]["position"]["lat"][0:]
-            project_lon = bigjson["info"]["location"]["position"]["lng"][0:]
+                # don't store city-wide projects
+                neighborhood =  bigjson["info"]["location"]["location_id"]
             
-
-            # store everything
-            data = {
-              'source' : "Change By Us",
-              'project' : project_name,
-              'url' : project_url,
-              'latitude' : project_lat,
-              'longitude' : project_lon,
-            }
-
-            scraperwiki.sqlite.save(unique_keys=['project'], data=data)            
+                if neighborhood == -1:
+                    store_it = 0
+                    # we don't want to put city-wide projects on the map, they're cool projects but it's confusing 
     
+                # this is not very elegant, but I'm confused about dealing with this json-like string
+                jitterlat = random.uniform(-0.02, 0.02)
+                jitterlon = random.uniform(-0.015, 0.015)
+                
+                project_lat = float(bigjson["info"]["location"]["position"]["lat"][0:]) + jitterlat
+                project_lon = float(bigjson["info"]["location"]["position"]["lng"][0:]) + jitterlon
+            
+                # store everything
+                if store_it == 1:
+                    data = {
+                      'source' : "Change By Us",
+                      'project' : project_name,
+                      'url' : project_url,
+                      'latitude' : project_lat,
+                      'longitude' : project_lon,
+                    }
+
+                    scraperwiki.sqlite.save(unique_keys=['project'], data=data)  
+                     
 # --------
 
-print "Getting ioby projects..."
 ioby()
 
-print "Getting Change By Us projects..."
 changebyus()
